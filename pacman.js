529e8d8581c7152d3803c73de70a5884dc64518a
Ashley Hackett <114507502+AshleyHackettCode@users.noreply.github.com>
2022-10-28 14:53:54 -0400

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,8 +1,8 @@
 var pos = 0;
 let pageWidth = window.innerWidth;
 const pacArray = [
-  ["./images/pacman1.png", "./images/pacman2.png"],
-  ["./images/pacman3.png", "./images/pacman4.png"],
+  ["PacMan1.png", "PacMan2.png"],
+  ["PacMan3.png", "PacMan4.png"],
 ];
 var direction = 0;
 var focus = 0;
